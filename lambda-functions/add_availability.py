cd31531791c7f32f8f5d8dd1597a94559146f823
DishaPatel126 <disha126p@gmail.com>
2025-07-19 19:53:20 -0300

    apigateway

--- lambda-functions/add_availability.py
+++ lambda-functions/add_availability.py
@@ -1,5 +1,42 @@
 import boto3
 import json
+import jwt
+import urllib.request
+
+COGNITO_USERPOOL_ID = "us-east-1_eSpO3xVoo"
+APP_CLIENT_ID = "2d8qd5us515158b4knocrgapoh"
+REGION = "us-east-1"
+
+# Download and cache JWKs
+keys_url = f"https://cognito-idp.{REGION}.amazonaws.com/{COGNITO_USERPOOL_ID}/.well-known/jwks.json"
+with urllib.request.urlopen(keys_url) as f:
+    response = f.read()
+    jwks = json.loads(response.decode("utf-8"))["keys"]
+
+def verify_jwt(token):
+    headers = jwt.get_unverified_header(token)
+    kid = headers["kid"]
+    key = next((k for k in jwks if k["kid"] == kid), None)
+    if not key:
+        raise Exception("Public key not found in JWKs")
+
+    public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(key))
+    decoded = jwt.decode(token, public_key, algorithms=["RS256"], audience=APP_CLIENT_ID)
+    return decoded
+
+def get_event_body(event):
+    if "Authorization" not in event.get("headers", {}):
+        raise Exception("Missing Authorization header")
+    token = event["headers"]["Authorization"].split(" ")[1]
+    decoded_token = verify_jwt(token)
+    user_id = decoded_token["sub"]
+
+    if event.get("body"):
+        body = json.loads(event["body"])
+    else:
+        body = event
+    return body, user_id
+
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Availability')
